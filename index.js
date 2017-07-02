7456930a7912852178416fdb196bddda6cb65785
code-miguel <miguelcode59@gmail.com>
2017-07-02 19:20:39 -0400

    hid password

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Nightmare = require('nightmare');
 new Nightmare()
   .goto('https://twitter.com/login')
   .type('input[class="js-username-field email-input js-initial-focus"]', 'lukenewben33k3@gmail.com') // Substitute with your username
-  .type('input[class="js-password-field"]', 'college2011math') // Substitute with your password
+  .type('input[class="js-password-field"]', 'xxxxxx') // Substitute with your password
   .click('button[class="submit EdgeButton EdgeButton--primary EdgeButtom--medium"]')
 
   .wait('span[class="ProfileCardStats-statValue"]')
